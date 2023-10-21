3e0d4c4ad608dfa4f357c949af3ca3344d8d349f
Dmitriy Sosnovskiy <so.d18@yandex.ru>
2023-10-21 18:47:31 +0300

    Добавил сериализацию для LocalDate

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.insert-koin:koin-ktor:$koin_version")
